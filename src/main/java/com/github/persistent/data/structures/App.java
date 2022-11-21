fdb41263e72fcbbf95475963aca303caa12f8de7
Lacquered <marshalnsk22854@gmail.com>
2022-11-21 09:24:22 +0700

    Fix code style

--- src/main/java/com/github/persistent/data/structures/App.java
+++ src/main/java/com/github/persistent/data/structures/App.java
@@ -4,10 +4,9 @@ package com.github.persistent.data.structures;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String[] args)
-    {
+public class App {
+
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
